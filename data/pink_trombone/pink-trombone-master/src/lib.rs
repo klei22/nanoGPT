93b588081e5e9a28012ae32cdb87ec970334d55a
klei22 <kauna@avalonwest.tech>
2025-07-19 16:49:00 -0700

    Add pinktrombone dataset folder

---
+++ data/pink_trombone/pink-trombone-master/src/lib.rs
@@ -0,0 +1,14 @@
+mod glottis;
+mod math;
+mod noise;
+mod noise_gen;
+mod rng;
+mod tract;
+mod tract_shaper;
+mod transient;
+mod trombone;
+mod turbulence;
+
+pub use noise::NoiseSource;
+pub use trombone::PinkTrombone;
+pub use turbulence::TurbulencePoint;
