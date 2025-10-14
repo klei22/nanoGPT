306d54267e4e0699944f3ddf13add7c352d0479f
Evans Jiang <xinting@umich.edu>
2025-10-14 16:12:48 +0000

    update val constraint

--- optimization_and_search/nsga_search/interactive_generation_scatter.py
+++ optimization_and_search/nsga_search/interactive_generation_scatter.py
@@ -820,9 +820,9 @@ def main():
     import argparse
     
     parser = argparse.ArgumentParser(description="Create Interactive Generational Scatter Plots")
-    parser.add_argument("--ckpt_base", type=str, default="ckpts/infi_attn_exp_iter20k/1010_1729_ckpt_gen", help="Path to the evolution log file")
-    parser.add_argument("--start_gen", type=int, default=1, help="Starting generation index")
-    parser.add_argument("--end_gen", type=int, default=40, help="Ending generation index")
+    parser.add_argument("--ckpt_base", type=str, default="ckpts/infi_medium/ckpt_gen", help="Path to the evolution log file")
+    parser.add_argument("--start_gen", type=int, default=0, help="Starting generation index")
+    parser.add_argument("--end_gen", type=int, default=50, help="Ending generation index")
     parser.add_argument("--output", type=str, default="htmls/interactive_generational_scatter.html", help="Output HTML file path")
     args = parser.parse_args()
     
