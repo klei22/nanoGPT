6a00628636ab8515db77f9db245443d23716c0c2
Kauna <16511995+klei22@users.noreply.github.com>
2025-12-23 14:46:33 -0800

    Add scripts compatible with FLORES-200

---
+++ data/flores200-res/phoneticize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+python3 utils/en2ipa.py text_eng_Latn.txt --mode text --output_file ipa_text_eng_Latn.txt
+# utils/en2ipa_wrapper.sh
+# utils/ja2ipa.py
+# utils/ko_en_to_ipa.py
+# utils/zh_to_ipa.py
