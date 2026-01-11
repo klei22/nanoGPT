ee2ca2162ae774f72564dc2739c227f191ee0636
Kauna <16511995+klei22@users.noreply.github.com>
2026-01-11 09:13:16 -0800

    Add option to store tokenizer outputs in subdir

--- data/template/prepare.py
+++ data/template/prepare.py
@@ -65,7 +65,7 @@ def parse_arguments():
 
     # Additional options
     parser.add_argument("-T", "--track_token_counts", action="store_true", help="Track how often each token appears and store in meta.pkl")
-    parser.add_argument("--output_tokenization_subdir", action="store_true",
+    parser.add_argument("-s", "--output_tokenization_subdir", action="store_true",
                         help="Write meta.pkl/train.bin/val.bin into a subdirectory named after the selected tokenization method")
 
     return parser.parse_args()
